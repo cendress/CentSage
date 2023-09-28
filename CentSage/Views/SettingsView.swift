83b336cb4437e93ce1978f7d306ae3eb0efb5c3b
cendress <95492688+cendress@users.noreply.github.com>
2023-09-28 13:06:45 -0400

    Setup settings view

--- CentSage/Views/SettingsView.swift
+++ CentSage/Views/SettingsView.swift
@@ -5,17 +5,18 @@
 //  Created by Christopher Endress on 9/25/23.
 //
 
+import CoreData
 import SwiftUI
 
 struct SettingsView: View {
+  @Environment(\.managedObjectContext) private var viewContext
   @EnvironmentObject var themeProvider: ThemeProvider
-  @State private var notificationsEnabled = true
+  
+  @State private var showingAlert = false
   
   var body: some View {
     NavigationView {
       Form {
-        Toggle("Notifications", isOn: $notificationsEnabled)
-        
         Picker("Theme", selection: $themeProvider.isDarkMode) {
           Text("Light").tag(false)
           Text("Dark").tag(true)
@@ -32,8 +33,48 @@ struct SettingsView: View {
         } header: {
           Text("Legal")
         }
+        
+        Section {
+          Button(action: {
+            showingAlert = true
+          }) {
+            Text("Delete My Data")
+              .foregroundColor(.red)
+          }
+          .alert(isPresented: $showingAlert) {
+            Alert(title: Text("Are you sure?"),
+                  message: Text("This will permanently delete all your data. This action can't be undone."),
+                  primaryButton: .destructive(Text("Delete")) {
+              deleteUserData()
+            },
+                  secondaryButton: .cancel()
+            )
+          }
+        } header: {
+          Text("Data Management")
+        }
       }
       .navigationTitle("Settings")
+    }
+  }
+  
+  func deleteUserData() {
+    let entities = ["Budget", "SavingsGoal", "Transaction"]
+    
+    for entity in entities {
+      let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: entity)
+      let batchDeleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+      do {
+        try viewContext.execute(batchDeleteRequest)
+      } catch {
+        
+      }
+    }
+    
+    do {
+      try viewContext.save()
+    } catch {
+     
     }
   }
 }
