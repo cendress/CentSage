f8a57571869fbb6c52c373bd0d4d89ab6789f9e8
cendress <95492688+cendress@users.noreply.github.com>
2023-09-29 12:13:30 -0400

    Fix final bug

--- CentSage/Views/SavingsGoalViews/SavingsGoalsListView.swift
+++ CentSage/Views/SavingsGoalViews/SavingsGoalsListView.swift
@@ -12,13 +12,9 @@ struct SavingsGoalsListView: View {
   @Environment(\.managedObjectContext) private var viewContext
   @StateObject private var viewModel: SavingsGoalsViewModel
   
-  @State private var selectedGoal: SavingsGoal?
   @State private var showingNewGoalView = false
-  
-  @State private var showingUpdateView = false
+  @State private var selectedGoal: SavingsGoal?
   @State private var refreshTrigger = false
-  
-  @State private var displayedGoal: SavingsGoal?
   
   init(context: NSManagedObjectContext) {
     _viewModel = StateObject(wrappedValue: SavingsGoalsViewModel(context: context))
@@ -28,9 +24,36 @@ struct SavingsGoalsListView: View {
     NavigationView {
       VStack {
         if viewModel.goals.isEmpty {
-          emptyGoalsView
+          VStack {
+            Spacer()
+            Image(systemName: "plus.circle.fill")
+              .resizable()
+              .scaledToFit()
+              .frame(width: 100, height: 100)
+              .foregroundColor(.gray)
+              .padding()
+            Text("No goals yet!")
+              .font(.headline)
+              .padding(.bottom, 1)
+            Text("Tap on the + button to add a new goal.")
+              .font(.subheadline)
+              .foregroundColor(.gray)
+            Spacer()
+          }
+          .padding()
         } else {
-          goalsListView
+          List {
+            ForEach(viewModel.goals) { goal in
+              Button(action: {
+                selectedGoal = goal
+              }) {
+                SavingsGoalRow(goal: goal)
+                  .padding()
+              }
+              .buttonStyle(PlainButtonStyle())
+            }
+            .onDelete(perform: viewModel.deleteGoals)
+          }
         }
       }
       .navigationTitle("Savings Goals")
@@ -52,59 +75,13 @@ struct SavingsGoalsListView: View {
       .onAppear {
         viewModel.fetchGoals()
       }
-      .sheet(isPresented: $showingUpdateView, onDismiss: {
-        displayedGoal = nil
-      }) {
-        if let goal = displayedGoal {
-          UpdateSavingsView(viewModel: viewModel, refreshTrigger: $refreshTrigger, goal: goal)
-            .environment(\.managedObjectContext, viewContext)
-        }
+      .sheet(item: $selectedGoal, onDismiss: {
+        viewModel.fetchGoals()
+      }) { selectedGoal in
+        UpdateSavingsView(viewModel: viewModel, refreshTrigger: $refreshTrigger, goal: selectedGoal)
+          .environment(\.managedObjectContext, viewContext)
       }
     }
-    .onChange(of: showingUpdateView) { newValue in
-      if newValue {
-        displayedGoal = selectedGoal
-      }
-    }
-  }
-  
-  var emptyGoalsView: some View {
-    VStack {
-      Spacer()
-      
-      Image(systemName: "plus.circle.fill")
-        .resizable()
-        .scaledToFit()
-        .frame(width: 100, height: 100)
-        .foregroundColor(.gray)
-        .padding()
-      Text("No goals yet!")
-        .font(.headline)
-        .padding(.bottom, 1)
-      Text("Tap on the + button to add a new goal.")
-        .font(.subheadline)
-        .foregroundColor(.gray)
-      
-      Spacer()
-    }
-    .padding()
-  }
-  
-  var goalsListView: some View {
-    List {
-      ForEach(viewModel.goals) { goal in
-        Button(action: {
-          selectedGoal = goal
-          showingUpdateView = true
-        }) {
-          SavingsGoalRow(goal: goal)
-            .padding()
-        }
-        .buttonStyle(PlainButtonStyle())
-      }
-      .onDelete(perform: viewModel.deleteGoals)
-    }
-    .id(refreshTrigger)
   }
 }
 
